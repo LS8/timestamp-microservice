3a351cc6798e81c7e2595b906cf6a4984440f30a
schoettker <lennartschoettker@hotmail.com>
2017-03-16 13:41:06 +0100

    code cleanup

--- index.js
+++ index.js
@@ -3,22 +3,18 @@ var moment = require('moment');
 var app = express();
 
 app.get('*', function(req, res) {
-  var param = req.params["0"].slice(1);
-  var date = {
-    unix: null,
-    natural: null
-  }
-  // Case 1: param is a unix timestamp
+  var param = req.params["0"].slice(1),
+      date = { unix: null, natural: null };
 
-  // Case 2: param is a natural language date
+  // Case 1: param is a natural language date
   var naturalDate = moment(param).toLocaleString();
-
   if (naturalDate && naturalDate !== "Invalid date") {
-    console.log(naturalDate);
     var unixDate = +new Date(moment(param).toISOString()) / 1000;
     date.natural = naturalDate.slice(0, 15);
     date.unix = unixDate;
-  } else {
+  }
+  // Case 2: param is a unix timestamp
+  else {
     var isoDate = new Date(+param*1000);
     var unixDate  = Date.parse(isoDate);
     if (unixDate) {
@@ -26,10 +22,8 @@ app.get('*', function(req, res) {
       date.natural = moment(isoDate).toLocaleString().slice(0, 15);
     }
   }
-
-
   res.send(JSON.stringify(date));
   res.end();
-})
+});
 
 app.listen(3000);
