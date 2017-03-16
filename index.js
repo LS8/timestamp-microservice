c7b7d3555fd33d3423d58978635551fa5339ab48
LS8 <lennartschoettker@hotmail.com>
2017-03-16 21:30:23 +0100

    refactor code to utilize functions (allows for better testing)

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 var express = require('express');
 var moment = require('moment');
 var app = express();
+var date = { unix: null, natural: null };
 
 app.get('*', function(req, res) {
-  var param = req.params["0"].slice(1),
-      date = { unix: null, natural: null };
+  var param = req.params["0"].slice(1);
 
   // Case 1: param is a natural language date
-  var naturalDate = moment(param).toLocaleString();
-  if (naturalDate && naturalDate !== "Invalid date") {
-    var unixDate = +new Date(moment(param).toISOString()) / 1000;
+  var naturalDate = processNaturalDate(param);
+  if (naturalDate) {
+    var unixDate = +new Date(moment(naturalDate).toISOString()) / 1000;
     date.natural = naturalDate.slice(0, 15);
     date.unix = unixDate;
   }
   // Case 2: param is a unix timestamp
   else {
-    var isoDate = new Date(+param*1000);
-    var unixDate  = Date.parse(isoDate);
-    if (unixDate) {
-      date.unix = unixDate/1000;
-      date.natural = moment(isoDate).toLocaleString().slice(0, 15);
+    var dates = processUnixDate(param);
+    if (dates) {
+      date.natural = dates.naturalDate;
+      date.unix = dates.unixDate;
     }
   }
   res.send(JSON.stringify(date));
@@ -27,3 +26,27 @@ app.get('*', function(req, res) {
 });
 
 app.listen(process.env.PORT || 8080);
+
+function processNaturalDate(dateString) {
+  var naturalDate = moment(dateString).toLocaleString();
+  if (naturalDate && naturalDate !== "Invalid date") {
+    return naturalDate;
+  } else {
+    return void 0;
+  }
+}
+
+function processUnixDate(unixTimestamp) {
+  var naturalDate = new Date(+unixTimestamp*1000);
+  var unixDate  = Date.parse(naturalDate);
+  if (unixDate) {
+    unixDate = unixDate/1000;
+    naturalDate = moment(naturalDate).toLocaleString().slice(0, 15);
+    return {
+      unixDate: unixDate,
+      naturalDate: naturalDate
+    }
+  } else {
+    return void 0;
+  }
+}
